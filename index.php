b8d77f29d188b370c8a584a99790b96bc3111445
shreyam <mshreya@infologitech.com>
2019-08-12 11:47:49 -0400

    removed garbage code

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 	<title>this is a landing page</title>
 </head>
 <body>
-<<<<<<< HEAD
+
 this is a landing page
 <p>hi how are you</p>
-=======
+
 <h1>This is a landing page</h1>
 <a href="login.php">Click here to Login</a>
->>>>>>> 9ac18177b388a376344c414c77f30ec9d0bb4efb
+
 </body>
 </html>
